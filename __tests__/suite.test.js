0337ec1e656f58cea4c4d509130a7019e1494100
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-09-01 09:16:15 -0500

    feat: added instructions and test cases

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,10 @@
-const { func } = require('../exercise');
+const { pairTheSum } = require('../exercise');
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
+test('Testing the pairTheSum exercise', () => { 
+    expect(pairTheSum([])).toBeDefined();
+    expect(pairTheSum([5,7,2], 9)).toBe(true);
+    expect(pairTheSum([2,9,1], 8)).toBe(false);
+    expect(pairTheSum([3,3],6)).toBe(true);
+    expect(pairTheSum([2,11,8,7], 9)).toBe(true);
+    expect(pairTheSum([3,4,5,6], 2)).toBe(false)
  })
--- exercise.js
+++ exercise.js
@@ -1,9 +1,25 @@
+/* *Instructions* 
+
+    You will receive two arguments, the first one will be an array with a series of numbers and the second will be just a number
+    this number will be called the target as you need to form that target number with the integers provided by the array.
+
+    In summary you need to return true if the target number can be formed by the integers or false if it is impossible 
+    taking into account that you cannot repeat integers.
+
+    Good luck!
+
+*/
+
+
+
 /** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
+const pairTheSum = (numbers, target) => {
+    let res = false;
     /* Only make changes below this comment */
     
-    
     /* Only make changes below this comment */
+
+    return res;
 }
 
 
@@ -11,4 +27,4 @@ const func = (c) => {
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.pairTheSum = pairTheSum;
