0630a5a7401d5e44c63a2f8d3c074d653b4dafb5
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-19 18:27:22 -0500

    Fucntion and Testing

--- exercise.js
+++ exercise.js
@@ -10,21 +10,35 @@
 
 */
 
-
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const pairTheSum = (numbers, target) => {
-    let res = false;
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
+  let res = false;
+  /* Only make changes below this comment */
 
-    return res;
-}
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      if (typeof numbers[i] !== "number" || typeof numbers[j] !== "number") {
+        return console.warn("Please only number values in array");
+      } else if (typeof target !== "number") {
+        return console.warn("Please input target value");
+      } else {
+        let add = numbers[i] + numbers[j];
+        if (add === target) {
+          return true;
+        }
+      }
+    }
+  }
+  /* Only make changes below this comment */
+  return res;
+};
 
-
-
-
+console.log(pairTheSum([])); //
+console.log(pairTheSum([5, 7, 2], 9)); //true
+console.log(pairTheSum([2, 9, 1], 8)); //false
+console.log(pairTheSum([3, 3], 6)); //true
+console.log(pairTheSum([2, 11, 8, 7], 9)); //true
+console.log(pairTheSum([3, 4, 5, 6], 2)); //false
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.pairTheSum = pairTheSum;
