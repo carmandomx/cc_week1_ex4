729eb03b5a15384defdb06352ecb164e9c1a32b4
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-09-20 16:03:21 -0500

    Solved exercise 4

--- exercise.js
+++ exercise.js
@@ -17,6 +17,24 @@ const pairTheSum = (numbers, target) => {
     let res = false;
     /* Only make changes below this comment */
     
+    for (let i = 0; i < numbers.length; i++) {
+        // take the first value in the numbers array
+        const value_1 = numbers[i];
+        // loop through the rest of the values
+        for (let j = i+1; j < numbers.length; j++) {
+            const value_2 = numbers[j];
+            // add both values
+            const pairSum = value_1 + value_2
+
+            if (pairSum === target) {
+                // check if the sum equals the target value
+                res = true;
+            } else {
+                // otherwise keep looping with the next value
+                continue;
+            }
+        }
+    }
     /* Only make changes below this comment */
 
     return res;
