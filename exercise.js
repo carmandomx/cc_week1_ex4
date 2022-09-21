46855450ea4dee9e1734889f72814c1dc3c768ae
Cesar Perez <cesar.perez@itksquare.edu.mx>
2022-09-21 15:51:58 -0400

    Exercise IV - Pair the sum. Cesar Perez

--- exercise.js
+++ exercise.js
@@ -17,6 +17,37 @@ const pairTheSum = (numbers, target) => {
     let res = false;
     /* Only make changes below this comment */
     
+    let targetVs = 0;                                                   // Variable to be used as comparator against the target.
+    // let numbers = numbers;                                            // Local array of numbers provided.
+
+    // Two for loops to match each number in the array with the other numbers. 
+    for (let index = 0; index < numbers.length; index++) {
+        
+        const element = numbers[index];                               // Element will receive the value at each array position. 
+        targetVs = element;                                             // For each number the sum starts with itself.
+
+        for (let indexJ = 0; indexJ < numbers.length; indexJ++) {
+            
+            const elementJ = numbers[indexJ];                         // ElementJ will receive the value at each array position. 
+            
+            
+            if ((index !== indexJ) &&                                   // Avoid using the same integer in comparisons. 
+                (targetVs < target) &&                                  // Only sum if target it is still greater than. 
+                ((targetVs + elementJ) <= target ) ) {                  // Only sum if target will be less or equal than the sum. 
+                    targetVs += elementJ;                               // Sum two numbers from the array.
+                }
+            }           
+        
+        // If target was reached, return true.
+        if (targetVs === target) {                                      
+            res = true;
+            break;
+        }
+        
+        }
+
+        
+
     /* Only make changes below this comment */
 
     return res;
