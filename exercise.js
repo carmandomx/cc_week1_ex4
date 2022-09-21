ec94d6a45cfa03f8e0be2d18714eef572413469a
Brandon Hernandez <Brandon.Hernandez@itksquare.edu.mx>
2022-09-21 16:08:13 -0500

    final test and comments

--- exercise.js
+++ exercise.js
@@ -16,7 +16,15 @@
 const pairTheSum = (numbers, target) => {
     let res = false;
     /* Only make changes below this comment */
-    
+    let map = new Map(); // I decided i needed to previsualize what was going on to keep track of my mistakes so I created a new object
+    for (let i = 0; i < numbers.length; i++) { //I first started the for loop to check the numbers in the array
+        let num1 = numbers[i]; // created a new variable with the index of the array
+        let num2 = target - num1; // here is the important part, the program will be tasked to find the number remaining by substracting the target from the current position
+        if (map.has(num2)) { // this condition will  allow us to know if the remaining pair of the sum exists in the array
+            return res = true; // then the result will be true
+        }
+        map.set(num1, i); // the program will continously look for the the second pair, but will store the value of the first pair to no loop indeterminately
+    }
     /* Only make changes below this comment */
 
     return res;
