47a2089cd27d91639fa07776dab1856e8abe5dd3
Jonathan castillo <jonathan.castillo.itksquare.edu.mx>
2022-09-21 15:18:56 -0500

    Upload the excercise file

--- exercise.js
+++ exercise.js
@@ -16,7 +16,21 @@
 const pairTheSum = (numbers, target) => {
     let res = false;
     /* Only make changes below this comment */
-    
+    // Let's create a for starting at 0 and increment 1 and 1 so that we can reach the length of the array
+    for (let i = 0; i <= numbers.length; i++) {
+        // Let's create a for starting at 0 and increment 1 and 1 so that we can reach the length of the array, this one is to compare later all 1 by 1 the elements of the same array 
+        for (let j = i + 1; j <= numbers.length; j++) {
+            // Now we need to compare those values, in this case each element of the second loop will be compared with the actual element of the first loop, then the first loop increments and the second loop start again, and so on. 
+            // If in some point the sum of those values is equal to the target parameter, it is gonna return true
+            if (numbers[i] + numbers[j] == target) {
+                res = true;
+            }
+            // We need to compare if we are not repeating the same position of the array, if at some point they repeat themselves the sum is not going to count.
+            else if (numbers[i] === numbers[j]) {
+                res = false
+            }
+        }
+    }
     /* Only make changes below this comment */
 
     return res;
