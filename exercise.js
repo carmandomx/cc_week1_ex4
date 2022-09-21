edb51a7d42c9740ebe8399a7ffbce70ee6e31fdf
Tomas Garcia <Tomas.Garcia@itksquare.edu.mx>
2022-09-21 14:52:48 -0500

    Task done

--- exercise.js
+++ exercise.js
@@ -14,12 +14,33 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const pairTheSum = (numbers, target) => {
-    let res = false;
+
+    //Flag which indicates if theres exist a pair formed by two differents integers
+    let res = false; //Default value: false
+
     /* Only make changes below this comment */
+
+    // With this "for" to go over each number on the array
+    for (let i = 0; i < numbers.length - 1; i++) {
+
+        //This one will take the other number to compare
+        for (let j = i + 1; j < numbers.length; j++) {
+
+            /* Then do the sum and compares if matchs with the target */
+            if (numbers[i] + numbers[j] === target) {
+                
+                /* Sets true the flag to indicate that exist a pair in the array
+                which the resoult is the "target" number */
+                res = true;
+            }
+            
+        }
+        
+    }
     
     /* Only make changes below this comment */
 
-    return res;
+    return res; //Returns the resoult (true / false) value
 }
 
 
