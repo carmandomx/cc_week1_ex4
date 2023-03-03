dae6014b368a739ed31f9c6a8b1029ca9a52f8f2
Rita Torres <taraumara96@gmail.com>
2023-03-02 18:08:10 -0600

    CambieExercise

--- exercise.js
+++ exercise.js
@@ -16,7 +16,15 @@
 const pairTheSum = (numbers, target) => {
     let res = false;
     /* Only make changes below this comment */
-    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+          if (numbers[i] + numbers[j] === target) {
+            res= true; 
+        }
+    }
+    /* Only make changes below this comment */
+    }
+
     /* Only make changes below this comment */
 
     return res;
